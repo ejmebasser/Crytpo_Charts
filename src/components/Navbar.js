d852900a94017792f06ccd28f45e69d637488956
Emil Mebasser <emil@Emils-Mac-mini.local>
2022-07-27 18:38:22 -0700

    638

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { FaCoins } from 'react-icons/fa';
-import './Navbar.css'
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>
+    <Link to='/'>
       <div className='navbar'>
         <FaCoins className='icon' />
         <h1>
-          Coin <span className='purple'>Search</span>
+          CodeSmith <span className='purple'> Coin Search</span>
         </h1>
       </div>
-    </div>
+    </Link>
   );
 };
 
